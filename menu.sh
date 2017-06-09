c95fc81306e12ddc93f1a0eca16f42465afa6338
xiangbaloud <xiangbaloud@gmail.com>
2017-06-09 17:49:40 +0800

    Fix syntax error

--- menu.sh
+++ menu.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
+options=("apple" "banana" "orange")
 PS3='Please select a function: '
-options=("Option 1" "Option 2" "Option 3" "Quit")
 
 select opt in "${options[@]}"
 do
-    case $opt in
-        "Option 1")	echo "you chose choice 1";;
-        "Option 2")	echo "you chose choice 2";;
-        "Option 3")	echo "you chose choice 3";;
-        "Quit")         exit;;
-        *) 		echo invalid option;;
-    esac
+	case $REPLY in
+		1 )  echo "you chose $opt"; break;;
+		2 )  echo "you chose $opt"; break;;
+		3 )  echo "you chose $opt"; break;;
+		$((${#options[@]}+1)) ) echo "Goodbye!"; break;;
+		*)  echo invalid option;;
+	esac
 done
